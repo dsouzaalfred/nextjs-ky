1b17f6697cff5baf9b07b9e98d199656c805072e
Alfred DSouza <alfred.dsouza@mobile-5.com>
2019-11-25 16:59:05 +0000

    add polyfills for IE11

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 const withPlugins = require("next-compose-plugins");
 const withTM = require("next-transpile-modules");
 
+const nextConfig = {
+    webpack: config => {
+        const originalEntry = config.entry;
+        config.entry = async() => {
+            const entries = await originalEntry();
+            if (entries["main.js"]) {
+                entries["main.js"].unshift("./client/polyfills.js");
+            }
+            return entries;
+        }
+
+        return config;
+    }
+};
+
 module.exports = withPlugins(
     [
         [
@@ -9,4 +24,4 @@ module.exports = withPlugins(
                 transpileModules: ["ky", "ky-universal"],
             },
         ],
-    ]);
+    ], nextConfig);
